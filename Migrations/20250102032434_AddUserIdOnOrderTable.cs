95f9435702cd4b4f28ade8da632daa5c2cd17579
David Surya <suryadavid@ymail.com>
2025-01-02 14:48:19 +0700

    new: migration for adding user id on order table

---
+++ Migrations/20250102032434_AddUserIdOnOrderTable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdOnOrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "order_number",
+                table: "orders",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "user_id",
+                table: "orders",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "ix_orders_user_id",
+                table: "orders",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_orders_users_user_id",
+                table: "orders",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_orders_users_user_id",
+                table: "orders");
+
+            migrationBuilder.DropIndex(
+                name: "ix_orders_user_id",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "order_number",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "orders");
+        }
+    }
+}
